f0a84ab0b1ddb70755332b35a4b4df7467c98a88
Matt Kilcup <nevermindthelabel@gmail.com>
2019-10-31 21:21:18 -0700

    updates to layour wrapper component

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,41 @@
-import React, { useState } from 'react'
-import PropTypes from 'prop-types'
-import styled from 'styled-components'
+import React, { useState, Fragment } from "react"
+import PropTypes from "prop-types"
+import styled from "styled-components"
 
-import Header from './Header'
-import OverlayMenu from './OverlayMenu'
-import Hamburger from './Hamburger'
-import Footer from './Footer'
+import Header from "./Header"
+import OverlayMenu from "./OverlayMenu"
+import Hamburger from "./Hamburger"
+import Footer from "./Footer"
 
-import './layout.css'
-import 'bootstrap/dist/css/bootstrap-grid.css'
+import "./layout.css"
+import "bootstrap/dist/css/bootstrap-grid.css"
+
+const Primary = styled.main`
+  padding: 110px 0 0 0;
+`
+const Layout = ({ children }) => {
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  const handleOverlayMenu = () => {
+    setMenuOpen(!menuOpen)
+  }
+
+  return (
+    <Fragment>
+      <Hamburger />
+      <OverlayMenu />
+      <Primary id="primary" className="content-area">
+        <main id="main" className="site-main" role="main">
+          {children}
+        </main>
+      </Primary>
+      <Footer />
+    </Fragment>
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
