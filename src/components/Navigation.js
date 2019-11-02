4948927c76f5e4e47e6e6555ab5b7eafc914e75d
Matt Kilcup <nevermindthelabel@gmail.com>
2019-11-02 14:38:08 -0700

    added navigation menu title and dropdown menus

---
+++ src/components/Navigation.js
@@ -0,0 +1,39 @@
+import React, { Fragment } from 'react';
+import { Link } from 'gatsby';
+import PropTypes from 'prop-types';
+
+import { NavigationWrapper } from './styles/NavigationStyles';
+
+const Navigation = ({ menu }) => (
+  <NavigationWrapper>
+    <ul>
+      {menu.items.map((item, i) => (
+        <li key={i}>
+          <Link to={item.url} activeClassName="nav-active">
+            {item.title}
+          </Link>
+          {item.wordpress_children ? (
+            <Fragment>
+              <span>&#8964;</span>
+              <ul>
+                {item.wordpress_children.map((child, iChild) => (
+                  <li key={iChild}>
+                    <Link to={child.url} activeClassName="nav-active">
+                      {child.title}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </Fragment>
+          ) : null}
+        </li>
+      ))}
+    </ul>
+  </NavigationWrapper>
+);
+
+Navigation.propTypes = {
+  menu: PropTypes.object,
+};
+
+export default Navigation;
