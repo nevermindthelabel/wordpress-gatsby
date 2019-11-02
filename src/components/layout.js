8cdaad10cf8cec0919b408e4cfa0684d4beeadb8
Matt Kilcup <nevermindthelabel@gmail.com>
2019-11-01 21:45:10 -0700

    added props for hamburger component

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,29 +1,30 @@
-import React, { useState, Fragment } from "react"
-import PropTypes from "prop-types"
-import styled from "styled-components"
+import React, { useState, Fragment } from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
-import Header from "./Header"
-import OverlayMenu from "./OverlayMenu"
-import Hamburger from "./Hamburger"
-import Footer from "./Footer"
+import Header from './Header';
+import OverlayMenu from './OverlayMenu';
+import Hamburger from './Hamburger';
+import Footer from './Footer';
 
-import "./layout.css"
-import "bootstrap/dist/css/bootstrap-grid.css"
+import './layout.css';
+import 'bootstrap/dist/css/bootstrap-grid.css';
 
 const Primary = styled.main`
   padding: 110px 0 0 0;
-`
+`;
 const Layout = ({ children }) => {
-  const [menuOpen, setMenuOpen] = useState(false)
+  const [menuOpen, setMenuOpen] = useState(false);
 
   const handleOverlayMenu = () => {
-    setMenuOpen(!menuOpen)
-  }
+    setMenuOpen(!menuOpen);
+  };
 
   return (
     <Fragment>
-      <Hamburger />
+      <Hamburger handleOverlayMenu={handleOverlayMenu} />
       <OverlayMenu />
+      <Header />
       <Primary id="primary" className="content-area">
         <main id="main" className="site-main" role="main">
           {children}
@@ -31,11 +32,11 @@ const Layout = ({ children }) => {
       </Primary>
       <Footer />
     </Fragment>
-  )
-}
+  );
+};
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
-}
+};
 
-export default Layout
+export default Layout;
