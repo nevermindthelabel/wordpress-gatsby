333ddce1f24b2377b89d30724f52220aa51b4636
Matt Kilcup <nevermindthelabel@gmail.com>
2019-11-04 22:00:29 -0700

    added logic to click previous and next buttons

--- src/components/index/HeroSlider.js
+++ src/components/index/HeroSlider.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
 import { useInterval } from './useInterval';
 import Slide from './Slide';
@@ -39,7 +39,60 @@ const HeroSlider = () => {
     }
   `);
 
+  const [state, setState] = useState({
+    slides,
+    activeIndex: 0,
+    autoSlide: true,
+    interval: 10000
+  });
 
-  return <div>HeroSlider</div>;
+  // useInterval custom hook by Dan Abramov @overreacted.io
+
+  useInterval(
+    () => {
+      setState(prev => ({
+        ...prev,
+        activeIndex: (prev.activeIndex + 1) % prev.slides.length
+      }));
+    },
+    state.autoSlide ? state.interval : null
+  );
+
+  const handlePrevClick = () => {
+    setState(prev => ({
+      ...prev,
+      autoSlide: false,
+      activeIndex:
+        (prev.activeIndex - 1 + prev.slides.length) % prev.slides.length,
+    }));
+  };
+
+  const handleNextClick = () => {
+    setState(prev => ({
+      ...prev,
+      autoSlide: false,
+      activeIndex:
+        (prev.activeIndex + 1 + prev.slides.length) % prev.slides.length,
+    }));
+  };
+
+  return (
+    <SliderWrapper>
+      {slides.map((slide, i) => (
+        <Slide
+          key={slide.node.id}
+          slide={slide.node}
+          active={state.activeIndex === i}
+        />
+      ))}
+      <ButtonWrapper style={{ position: 'absolute', left: 0 }}>
+        <Button onClick={handlePrevClick}>&#9664;</Button>
+      </ButtonWrapper>
+      <ButtonWrapper style={{ position: 'absolute', right: 0 }}>
+        <Button onClick={handleNextClick}>&#9654;</Button>
+      </ButtonWrapper>
+    </SliderWrapper>
+  );
 };
+
 export default HeroSlider;
